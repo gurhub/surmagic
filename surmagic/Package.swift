e1287af6b57996c6efdaf6a6f648f33e26a0f16b
Muhammed Gurhan Yerlikaya <gurhanyerlikaya@gmail.com>
2021-04-25 14:22:02 +0300

    swift-argument-parser and the swift-tools-support-core update

--- surmagic/Package.swift
+++ surmagic/Package.swift
@@ -7,8 +7,8 @@ let package = Package(
     name: "surmagic",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.3.0"),
-        .package(url: "https://github.com/apple/swift-tools-support-core", from: "0.1.12"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.4.0"),
+        .package(url: "https://github.com/apple/swift-tools-support-core", from: "0.2.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -17,9 +17,7 @@ let package = Package(
                 dependencies: [
                     .product(name: "ArgumentParser", package: "swift-argument-parser"),
                     .product(name: "SwiftToolsSupport-auto", package: "swift-tools-support-core"),
-                ],
-                resources: [
-                    .copy("Surfile")]
+                ]
         ),
         .testTarget(
             name: "surmagicTests",
