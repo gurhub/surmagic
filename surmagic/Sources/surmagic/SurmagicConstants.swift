ba61a453f53f8cb85b0268eccbf679830d3af371
Muhammed Gurhan Yerlikaya <gurhanyerlikaya@gmail.com>
2021-04-25 14:21:22 +0300

    Added a new file for the constants

---
+++ surmagic/Sources/surmagic/SurmagicConstants.swift
@@ -0,0 +1,17 @@
+//
+//  SurmagicConstants.swift
+//  Surmagic
+//
+//  Created by Muhammed Gurhan Yerlikaya on 25.04.2021.
+//  Copyright © 2020 https://github.com/gurhub/surmagic.
+//
+
+
+import Foundation
+
+struct SurmagicConstants {
+    static let surfileName          = "Surfile"
+    static let surfileDirectory     = "Surmagic"
+    static let executablePath       = "/usr/bin/env"
+    static let archiveExtension     = ".xcarchive"
+}
