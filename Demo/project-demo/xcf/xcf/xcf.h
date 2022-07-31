728074cdc59c46f4370249049be854a17699c327
Muhammed Gurhan Yerlikaya <gurhanyerlikaya@gmail.com>
2022-07-31 18:16:06 +0300

    added project demo

---
+++ Demo/project-demo/xcf/xcf/xcf.h
@@ -0,0 +1,20 @@
+//
+//  xcf.h
+//  surmagic
+//
+//  Created by Muhammed Gurhan Yerlikaya on 12.11.2020.
+//  Copyright © 2020 https://github.com/gurhub/surmagic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for xcf.
+FOUNDATION_EXPORT double xcfVersionNumber;
+
+//! Project version string for xcf.
+FOUNDATION_EXPORT const unsigned char xcfVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <xcf/PublicHeader.h>
+
+#import <xcf/GreetingsManager.h>
+    
