53a385f2f9a857bc1be0211860f988178dd5bd88
FFirX <FFirX@users.noreply.github.com>
2022-12-28 10:57:27 +0800

    Starting with tools version 5.4.0 executable targets should be declared as 'executableTarget()'

--- surmagic/Package.swift
+++ surmagic/Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(name: "surmagic",
+        .executableTarget(name: "surmagic",
                 dependencies: [
                     .product(name: "ArgumentParser", package: "swift-argument-parser"),
                     .product(name: "SwiftToolsSupport-auto", package: "swift-tools-support-core"),
