a0260b68d6c3a8bd21e3d13ba6a0ffd20d09fca1
FFirX <FFirX@users.noreply.github.com>
2022-12-29 15:21:25 +0800

    Bump to v1.3.0, update binary file

--- surmagic/Sources/surmagic/main.swift
+++ surmagic/Sources/surmagic/main.swift
@@ -21,7 +21,7 @@ struct Surmagic: ParsableCommand {
                   """,
 
         // Commands can define a version for automatic '--version' support.
-        version: "1.2.7",
+        version: "1.3.0",
         
         // Subcommands
         subcommands: [`init`.self, xcf.self, env.self],
